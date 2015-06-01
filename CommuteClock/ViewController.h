763c315eeeec3a661504c1eeddd51e23f71c3bb1
Danny G <daniel.grenzowski@gmail.com>
2015-05-31 20:20:16 -0400

    Initial Commit

---
+++ CommuteClock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommuteClock
+//
+//  Created by Danny G on 2015-05-31.
+//  Copyright (c) 2015 DGInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CommuteClock/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CommuteClock
+//
+//  Created by Danny G on 2015-05-31.
+//  Copyright (c) 2015 DGInc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
